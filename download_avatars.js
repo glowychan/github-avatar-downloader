7e1b48e3afe4a3d0cb68fec8a2cfa0dc5244ae8c
glowychan <glochan.uog@gmail.com>
2017-07-31 15:11:55 +0000

    Verify GET request url is valid

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,12 @@
 var request = require('request');
+var GITHUB_USER = "glowychan";
+var GITHUB_TOKEN = "a0e2139a1431cd3a2604429330c76083e57260e4";
+
+var repoOwner = "jquery";
+var repoName = "jquery";
+
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
