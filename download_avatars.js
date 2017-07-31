b6a6abe07ea710dbb5a7a4e9e1a9119e68c5d6fc
glowychan <glochan.uog@gmail.com>
2017-07-31 16:02:37 +0000

    Create downloadImageByUrl function and test with hardcoded values

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = "glowychan";
 var GITHUB_TOKEN = "a0e2139a1431cd3a2604429330c76083e57260e4";
 
@@ -19,4 +20,24 @@ var callback =  function (error, response, body) {
                   data.forEach(person => console.log(person.avatar_url));
                 };
 
-console.log(getRepoContributors("jquery", "jquery", callback));
+// console.log(getRepoContributors("jquery", "jquery", callback));
+
+
+function downloadImageByURL(url, filePath) {
+  // ...
+  request.get(url)
+         .on('error', function (err) {
+          throw err;
+         })
+         .on('response', function(response) {
+          console.log('Response Status: ', response.statusMessage);
+          console.log('Content Type: ', response.headers['content-type']);
+          console.log('Downloading image...');
+         })
+         .pipe(fs.createWriteStream(filePath))
+         .on('close', function() {
+            console.log('Download complete.');
+          });
+}
+
+console.log(downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', './avatars/kvirani.jpg'));
