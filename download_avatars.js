cc301c91119c05e964ef303f4c4c354385ca28a5
glowychan <glochan.uog@gmail.com>
2017-07-31 15:04:51 +0000

    Add hard-coded values to test function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,11 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+// Invoke function with hard-coded values
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
