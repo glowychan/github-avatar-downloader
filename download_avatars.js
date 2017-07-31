4614867807ef656b535ee4af5bbe83db77f1cc6c
glowychan <glochan.uog@gmail.com>
2017-07-31 16:46:40 +0000

    Refactor code

--- download_avatars.js
+++ download_avatars.js
@@ -1,50 +1,54 @@
+// Modules needed for program
 var request = require('request');
 var fs = require('fs');
+
+// Credentials for making API request
 var GITHUB_USER = "glowychan";
 var GITHUB_TOKEN = "a0e2139a1431cd3a2604429330c76083e57260e4";
 
+// Arguments for command line
 var owner = process.argv[2];
 var repo = process.argv[3];
 
+// Function to get user avatar urls
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  if(repoOwner && repoName) {
-    var options = {
-    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: { 'User-Agent': 'GitHub Avatar Downloader - Student Project' }
+  if (repoOwner && repoName) {
+      var options = {
+        url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+        headers: { 'User-Agent': 'GitHub Avatar Downloader - Student Project'
+      }
     };
 
     request(options, cb);
-  } else {
-    console.log("Error processing request - please ensure the repo owner and name is entered");
-  }
 
+  } else {
+    console.log("Error processing request, Please ensure you enter the repo owner and name in the terminal");
+  }
 }
 
-var callback =  function (error, response, body) {
-                  console.log('error:', error);
-                  console.log('statusCode:', response && response.statusCode);
-                  var data = JSON.parse(body);
-                  data.forEach(function(person) {
-                    var path = `./avatars/${person.login}.jpg`;
-                    var url = `${person.avatar_url}`;
-                    return downloadImageByURL(url, path);
-                  });
-                };
+// Callback function to handle request for the given repo owner and name
+function callback(error, response, body) {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  var data = JSON.parse(body);
+  data.forEach(function(person) {
+    var path = `./avatars/${person.login}.jpg`;
+    var url = person.avatar_url;
+    return downloadImageByURL(url, path);
+  });
+};
 
-// console.log(getRepoContributors("jquery", "jquery", callback));
-
-
+// Function to download images to user's file directory
 function downloadImageByURL(url, filePath) {
-  // ...
   request.get(url)
          .on('error', function (err) {
-          throw err;
+            throw err;
          })
          .on('response', function(response) {
-          console.log('Response Status: ', response.statusMessage);
-          console.log('Content Type: ', response.headers['content-type']);
-          console.log('Downloading image...');
+            console.log('Response Status: ', response.statusMessage);
+            console.log('Content Type: ', response.headers['content-type']);
+            console.log('Downloading image...');
          })
          .pipe(fs.createWriteStream(filePath))
          .on('close', function() {
@@ -52,5 +56,6 @@ function downloadImageByURL(url, filePath) {
           });
 }
 
-console.log(getRepoContributors(owner, repo, callback));
+// console.log(getRepoContributors("jquery", "jquery", callback)); should retrieve images from the jquery repo
+console.log(getRepoContributors(owner, repo, callback)); // allow user to specify repo on the terminal
 
